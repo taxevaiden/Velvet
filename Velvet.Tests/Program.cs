3123f8d610ea75ea91b8b5202bce97e406482f42
taxevaiden <taxevaiden@gmail.com>
2025-07-22 02:29:32 -0400

    basic window handling ig

---
+++ Velvet.Tests/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Principal;
+using Velvet;
+
+namespace Velvet.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var win = new VelvetWindow("Hello, world!", 720, 720);
+
+            while (win.IsRunning())
+            {
+                win.PollEvents();
+            }
+
+            win.Destroy();
+        }
+    }
+}
---
+++ Velvet/VelvetWindow.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Principal;
+using SDL3;
+using static SDL3.SDL;
+
+namespace Velvet
+{
+    public class VelvetWindow
+    {
+        private IntPtr _window;
+        private bool _running = false;
+        private SDL_Event _e;
+
+        public VelvetWindow(string title, int width, int height)
+        {
+            if (!SDL_Init(SDL_InitFlags.SDL_INIT_VIDEO))
+            {
+                Console.WriteLine($"Unable to initialize SDL: {SDL_GetError()}");
+                return;
+            }
+
+            _window = SDL_CreateWindow(title, width, height, SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
+            if (_window == IntPtr.Zero)
+            {
+                Console.WriteLine($"Window creation failed: {SDL_GetError()}");
+                SDL_Quit();
+                return;
+            }
+
+            _running = true;
+        }
+
+        public bool IsRunning() { return _running; }
+
+        public bool PollEvents()
+        {
+            while (SDL_PollEvent(out _e))
+            {
+                if (_e.type == (uint)SDL_EventType.SDL_EVENT_QUIT)
+                {
+                    _running = false;
+                    return false;
+                }
+            }
+
+            SDL_Delay(16);
+
+            return true;
+        }
+
+        public void Destroy()
+        {
+            if (_window != IntPtr.Zero)
+            {
+                SDL_DestroyWindow(_window);
+                _window = IntPtr.Zero;
+            }
+            SDL_Quit();
+        }
+
+    }
+}
